57a6602e6bade0cc8538625975f12735a11b310a
Alexandre Drouin <alexandre.drouin1@gmail.com>
2014-05-13 10:30:51 -0400

    Removed example dependency list. Replaced by merge of builtin lists

--- pyscm/model/base.py
+++ pyscm/model/base.py
@@ -17,26 +17,20 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
-from ..utils import _class_to_string
-
 
 class ModelMixin:
     def __init__(self):
         self.binary_attributes = []
-        self._example_dependencies = []
 
     def add(self, binary_attribute):
         self.binary_attributes.append(binary_attribute)
-
-        if len(binary_attribute.example_dependencies) > 0:
-            self._example_dependencies += binary_attribute.example_dependencies
 
     def remove(self, index):
         del self.binary_attributes[index]
 
     @property
     def example_dependencies(self):
-        return list(self._example_dependencies)
+        return [d for ba in self.binary_attributes for d in ba.example_dependencies]
 
     def _to_string(self, separator=" "):
         return separator.join([str(a) for a in self.binary_attributes])
