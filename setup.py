64f24af06df5de9d34be49ef45fd87b8bc7fe3a0
Alexandre Drouin <alexandre.drouin.8@ulaval.ca>
2014-12-03 22:55:33 -0500

    Build popcount.c during setpu

--- setup.py
+++ setup.py
@@ -1,4 +1,10 @@
-from setuptools import setup, find_packages
+try:
+    from numpy import get_include as get_numpy_include
+except:
+    print "Numpy 1.6.2 or greater is required to install this package. Please install Numpy and try again."
+    exit()
+
+from setuptools import setup, find_packages, Extension
 setup(
     name = "pyscm",
     version = "0.1",
@@ -11,5 +17,8 @@ setup(
     description = "The Set Covering Machine in Python",
     license = "GPLv3",
     keywords = "set covering machine learning feature selection data",
-    url = "http://github.com/aldro61/pyscm"
+    url = "http://github.com/aldro61/pyscm",
+
+    # Cython Extension
+    ext_modules = [Extension("pyscm/scm/popcount", ["pyscm/scm/popcount.c"], include_dirs=[get_numpy_include()], extra_compile_args=["-march=native"])]
 )
