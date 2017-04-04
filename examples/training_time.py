a88fb82ee1855f3922e61acb217c2a1a74755aa8
Alexandre Drouin <alexandre.drouin.8@ulaval.ca>
2017-04-04 17:00:40 -0400

    Refactored the example code

--- examples/training_time.py
+++ examples/training_time.py
@@ -7,65 +7,65 @@ from pyscm.scm import SetCoveringMachineClassifier
 from sklearn.datasets import make_classification
 from sklearn.metrics import accuracy_score
 from time import time
-#logging.basicConfig(level=logging.DEBUG,
-#                    format="%(asctime)s.%(msecs)d %(levelname)s %(module)s - %(funcName)s: %(message)s")
 
-np.random.seed(42)
 
 def increase_n_features():
+    n_repeats = 5
+    n_bench_points = 5
     n_examples = 1000
     n_features = 100000
-    times = []
-    nfs = []
-    for p in np.linspace(0.01, 1.0, 10):
-        nf = int(p * n_features)
-        X, y = make_classification(n_samples=n_examples, n_features=nf, n_classes=2,
-                                   random_state=np.random.RandomState(42))
-        clf = SetCoveringMachineClassifier(model_type="conjunction", p=1.0, max_rules=100)
-        t = time()
-        clf.fit(X, y)
-        took = time() - t
-        times.append(took)
-        nfs.append(nf)
-        print nf
-        print took
-        print
+    avg_times = np.zeros(n_bench_points)
+    nfs = [int(n_features * p) for p in np.linspace(0.01, 1.0, n_bench_points)]
+    for _ in range(n_repeats):
+        times = []
+        for nf in nfs:
+            X, y = make_classification(n_samples=n_examples, n_features=nf, n_classes=2,
+                                       random_state=np.random.RandomState(42))
+            clf = SetCoveringMachineClassifier(model_type="conjunction", p=1.0, max_rules=100)
+            t = time()
+            clf.fit(X, y)
+            times.append(time() - t)
+        avg_times += np.array(times)
+    avg_times /= 1.0 * n_repeats
 
     plt.clf()
-    plt.plot(nfs, times)
-    plt.legend()
+    plt.plot(nfs, avg_times)
     plt.xlabel("n features")
     plt.ylabel("time (seconds)")
-    plt.title("Training time for 1000 <= n <= 100000 features (1000 examples)")
-    plt.show()
+    plt.title("Training time for %d <= n <= %d features (%d examples)" % (min(nfs), max(nfs), n_examples))
+    plt.savefig("n_features.png", bbox_inches="tight")
+
 
 def increase_n_examples():
-    n_examples = 100000
+    n_repeats = 5
+    n_bench_points = 5
+    n_examples = 10000
     n_features = 1000
-    times = []
-    n_exs = []
-    for p in np.linspace(0.01, 1.0, 10):
-        n_ex = int(p * n_examples)
-        X, y = make_classification(n_samples=n_ex, n_features=n_features, n_classes=2,
-                                   random_state=np.random.RandomState(42))
-        clf = SetCoveringMachineClassifier(model_type="conjunction", p=1.0, max_rules=100)
-        t = time()
-        clf.fit(X, y)
-        took = time() - t
-        times.append(took)
-        n_exs.append(n_ex)
-        print n_ex
-        print took
-        print
+    avg_times = np.zeros(n_bench_points)
+    n_exs = [int(n_examples * p) for p in np.linspace(0.01, 1.0, n_bench_points)]
+    for _ in range(n_repeats):
+        times = []
+        for n_ex in n_exs:
+            X, y = make_classification(n_samples=n_ex, n_features=n_features, n_classes=2,
+                                       random_state=np.random.RandomState(42))
+            clf = SetCoveringMachineClassifier(model_type="conjunction", p=1.0, max_rules=100)
+            t = time()
+            clf.fit(X, y)
+            times.append(time() - t)
+        avg_times += np.array(times)
+    avg_times /= n_repeats
 
     plt.clf()
-    plt.plot(n_exs, times)
-    plt.legend()
+    plt.plot(n_exs, avg_times)
     plt.xlabel("n examples")
     plt.ylabel("time (seconds)")
-    plt.title("Training time for 1000 <= n <= 100000 examples (1000 features)")
-    plt.show()
+    plt.title("Training time for %d <= n <= %d examples (%d features)" % (min(n_exs), max(n_exs), n_features))
+    plt.savefig("n_examples.png", bbox_inches="tight")
+
 
 if __name__ == '__main__':
+    logging.basicConfig(level=logging.DEBUG,
+                        format="%(asctime)s.%(msecs)d %(levelname)s %(module)s - %(funcName)s: %(message)s")
     increase_n_examples()
     increase_n_features()
+
